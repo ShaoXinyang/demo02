6ebd46ea2f7324cf2976a6a96db46140f2d8d5f7
Robin <14575454062@qq.com>
2020-12-19 10:34:40 +0800

    添加了循环功能

--- src/main/java/cn/tedu/HelloWorld.java
+++ src/main/java/cn/tedu/HelloWorld.java
@@ -3,5 +3,9 @@ package cn.tedu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int sum=0;
+        for (int i=0; i<10; i++){
+            System.out.println(sum);
+        }
     }
 }
